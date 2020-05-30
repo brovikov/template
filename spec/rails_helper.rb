64d3e46a96181a8e84f8d8b34543f2473f5f2bc9
Alexey Brovikov <alexey.brovikov@gmail.com>
2020-05-30 13:22:24 +0300

    Simplify data base cleaner settings

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -42,33 +42,15 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = false
 
   config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with(:truncation)
   end
 
-  config.before(:each) do
-    DatabaseCleaner.strategy = :transaction
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
   end
-
-  config.before(:each, :js => true) do
-    DatabaseCleaner.strategy = :truncation
-  end
-
-  config.before(:each) do
-    DatabaseCleaner.start
-  end
-
-  config.after(:each) do
-    DatabaseCleaner.clean
-  end
-
-  config.before(:all) do
-    DatabaseCleaner.start
-  end
-
-  config.after(:all) do
-    DatabaseCleaner.clean
-  end
-
 
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
